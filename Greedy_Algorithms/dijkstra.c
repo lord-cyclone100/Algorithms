a760dcd6b10ae815dd1de150e86e34e913ec09d1
lord-cyclone100 <sang66deb@gmail.com>
2024-05-21 23:10:38 +0530

    Updated dijkstra

--- Greedy_Algorithms/dijkstra.c
+++ Greedy_Algorithms/dijkstra.c
@@ -1,80 +1,94 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdbool.h>
 
-int distance[100];
-bool spt[100];
+int graph[10][10],n,pred[100],dist[100],sptset[100];
 
-int extractMin(bool spt[],int dist[],int n){
-	int min = 999,i,minIndex;
-	for(i=1;i<=n;i++){
-		if(spt[i]==false && dist[i]<=min){
-			min=dist[i];
-			minIndex=i;
-		}
-	}
-	return minIndex;
-}
-
-void printResult(int dist[],int n){
-	int i;
-	printf("Vertex		Shortest path from source\n");
-	for(i=1;i<=n;i++){
-		printf("%d\t\t%d\n",i,dist[i]);
-	}
-}
-
-void Dijkstra(int graph[10][10],int n){
-	int i,s,count;
-	printf("Enter source vertex:");
-	scanf("%d",&s);
-	for(i=1;i<=n;i++){
-		distance[i]=999;
-		spt[i]=false;
-	}
-	distance[s]=0;
-	count=1;
-	while(count<n){
-		int u = extractMin(spt,distance,n);
-		spt[u]=true;
-		for(i=1;i<=n;i++){
-			if(spt[i]==false && graph[u][i]!=0 && distance[i]>distance[u]+graph[u][i]){
-				distance[i]=distance[u]+graph[u][i];
-			}
-		}
-		count++;
-	}
-	printResult(distance,n);
-}
-
-
-
-int main(){
-	int n,graph[10][10],i,j,s;
+void readGraph(){
+	int i,j;
 	FILE *fp;
 	fp = fopen("dijkstra.txt","r");
-	if(fp == NULL){
+	if(fp==NULL){
 		printf("Error");
 		exit(1);
 	}
 	fscanf(fp,"%d",&n);
-	for(i=1;i<=n;i++){
-		for(j=1;j<=n;j++){
+	
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
 			fscanf(fp,"%d",&graph[i][j]);
 		}
 	}
 	fclose(fp);
-	for(i=1;i<=n;i++){
-		for(j=1;j<=n;j++){
+}
+
+void printGraph(){
+	int i,j;
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
 			printf("%d  ",graph[i][j]);
 		}
 		printf("\n");
 	}
-//	printf("Select a source:");
-//	scanf("%d",&s);
-	Dijkstra(graph,n);
-	
-	
-	
+}
+
+void initialize(){
+	int i;
+	for(i=0;i<n;i++){
+		dist[i]=999;
+		pred[i]=0;
+		sptset[i]=0;
+	}
+}
+
+int extractMin(){
+	int i,minindex,minvalue=999;
+	for(i=0;i<n;i++){
+		if(sptset[i]==0 && dist[i]<minvalue){
+			minindex=i;
+			minvalue=dist[i];
+		}
+	}
+	return minindex;
+}
+
+void Dijkstra(int start){
+	initialize();
+	int i,w,j;
+	dist[start]=0;
+	for(i=0;i<n;i++){
+		int u=extractMin();
+		sptset[u]=1;
+		for(w=0;w<n;w++){
+			if(graph[u][w] != 999){
+				if(sptset[w]==0 && graph[u][w]+dist[u]<dist[w]){
+					dist[w]=graph[u][w]+dist[u];
+					pred[w]=u;
+				}
+			}
+		}
+	}
+}
+
+void printShortestPath(){
+	int i,w;
+	for(i=1;i<n;i++){
+		printf("S to %c:%d\n",'A'+i,dist[i]);
+		w=i;
+		while(w!=0){
+			printf("%c<-",'A'+w);
+			w=pred[w];
+		}
+		printf("S");
+		printf("\n\n");
+	}
+}
+
+int main(){
+	int i;
+	readGraph();
+	printGraph();
+	Dijkstra(0);
+	printf("Shortest distances\n");
+	printShortestPath();
 	return 0;
 }
