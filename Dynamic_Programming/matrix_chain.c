3ad6c72160448ae9bf8e1e917c39293478d7a9a5
lord-cyclone100 <sang66deb@gmail.com>
2024-05-22 15:52:21 +0530

    Updated codes

--- Dynamic_Programming/matrix_chain.c
+++ Dynamic_Programming/matrix_chain.c
@@ -1,92 +1,80 @@
 #include <stdio.h>
+#define INT_MAX 2147483647;
 
-//C[i,j]=min(C[i,k]+C[k+1,j]+p[i-1]*p[k]*p[j]    i<=k<j
-int table[100][100],s[100][100];
+int p[100],cost[10][10],kvalue[10][10],n;
 
-int minElement(int a[],int size){
-	int small = 0,i;
-	for(i=1;i<size;i++){
-		if(a[i]<a[small]){
-			small = i;
-		}
-	}
-	return small+1;
+void initialize(){
+    int i,j;
+    for(i=1;i<n;i++){
+        for(j=1;j<n;j++){
+            if(i>=j){
+                cost[i][j]=0;
+                kvalue[i][j]=0;
+            }
+        }
+    }
 }
 
-void matrixChain(int p[],int n){
-	int i,j,k,l;
-	for(i=1;i<=n;i++){
-		for(j=1;j<=n;j++){
-			if(i>=j){
-				table[i][j]=0;
-				s[i][j]=0;
-			}
-		}
-	}
-	for(l=1;l<n;l++){
-		for(i=1;i<=n;i++){
-			for(j=1;j<=n;j++){
-				if(j-i==l){
-					int arr[l],m=0,k_values[l],q=0;
-					for(k=i;k<j;k++){
-						arr[m++]=table[i][k]+table[k+1][j]+p[i-1]*p[k]*p[j];
-						k_values[q++]=k;
-						table[i][j]=arr[(minElement(arr,sizeof(arr)/sizeof(int)))-1];
-						s[i][j]=k_values[(minElement(arr,sizeof(arr)/sizeof(int)))-1];
-					}
-				}
-			}
-		}
-	}
+void matrixChain(){
+    int i,j,k,l;
+    initialize();
+    for(l=2;l<=n;l++){
+        for(i=1;i<=n-l+1;i++){
+            j=i+l-1;
+            cost[i][j]=INT_MAX;
+            for(k=i;k<=j;k++){
+                if(cost[i][j]>cost[i][k]+cost[k+1][j]+(p[i-1]*p[k]*p[j])){
+                    cost[i][j]=cost[i][k]+cost[k+1][j]+(p[i-1]*p[k]*p[j]);
+                    kvalue[i][j]=k;
+                }
+            }
+        }
+    }
 }
 
-void printCostMtx(int n){
-	int i,j;
-	for(i=1;i<=n;i++){
-		for(j=1;j<=n;j++){
-			printf("%d ",table[i][j]);
-		}
-		printf("\n");
-	}
+void printCostMtx(){
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            printf("%d   ",cost[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-void printKMtx(int n){
-	int i,j;
-	for(i=1;i<=n;i++){
-		for(j=1;j<=n;j++){
-			printf("%d ",s[i][j]);
-		}
-		printf("\n");
-	}
+void printkvalues(){
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            printf("%d   ",kvalue[i][j]);
+        }
+        printf("\n");
+    }
 }
 
-void printParenthesise(int s[100][100],int i,int j){
-	if(i==j){
-		printf("A%d",i);
-	}
-	else{
-		printf("(");
-		printParenthesise(s,i,s[i][j]);
-		printParenthesise(s,s[i][j]+1,j);
-		printf(")");
-	}
-	
+void optimalParenthesise(int s[10][10],int i,int j){
+    if (i==j){
+        printf("A%d",i);
+    }
+    else{
+        printf("(");
+        optimalParenthesise(s,i,s[i][j]);
+        optimalParenthesise(s,s[i][j]+1,j);
+        printf(")");
+    }
 }
 
 int main() {
-    int p[100],i,n;
-    printf("Enter the number of matrices:");
+    int i;
+    printf("Enter no of matrices:");
     scanf("%d",&n);
-    printf("Enter the order array:");
+    printf("Enter matrix orders:");
     for(i=0;i<=n;i++){
-    	scanf("%d",&p[i]);
-	}
-	matrixChain(p,n);
-	printCostMtx(n);
-	printf("\n");
-	printKMtx(n);
-	printf("\n");
-	printParenthesise(s,1,n);
+        scanf("%d",&p[i]);
+    }
+    matrixChain();
+    printCostMtx();
+	printkvalues();
+    optimalParenthesise(kvalue,1,n);
     return 0;
 }
-
